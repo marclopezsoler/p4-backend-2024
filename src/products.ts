e82213470b2d1e31cfb3dcf4cf56b98e5c6c72c1
Marc <marcls.arbucies@gmail.com>
2024-05-28 21:49:55 +0200

    Orders and products endpoints completed

--- src/products.ts
+++ src/products.ts
@@ -12,7 +12,10 @@ const productsBodySchema = z.object({
   price: z.number(),
   description: z.string().min(15).max(255),
   color: z.string().min(3).max(25),
+  sellerId: z.number(),
 });
+
+const partialProductsBodySchema = productsBodySchema.partial();
 
 productsRouter.get(
   "/",
@@ -36,6 +39,59 @@ productsRouter.get(
       where: { id: productId },
     });
     send(res).ok({ product });
+  })
+);
+
+productsRouter.post(
+  "/",
+  catchErrors(async (req, res) => {
+    const data = productsBodySchema.parse(req.body);
+    const product = await prisma.product.create({ data });
+    send(res).createdOk({
+      msg: `Id from new product: ${product.id}`,
+      product,
+    });
+  })
+);
+
+productsRouter.put(
+  "/:id",
+  catchErrors(async (req, res) => {
+    const { id: productId } = idParamsSchema.parse(req.params);
+    const bodyCheck = partialProductsBodySchema.parse(req.body);
+
+    const updatedProduct = await prisma.product.update({
+      where: { id: productId },
+      data: bodyCheck,
+    });
+
+    send(res).ok(updatedProduct);
+  })
+);
+
+productsRouter.delete(
+  "/:id",
+  catchErrors(async (req, res) => {
+    const { id: productId } = idParamsSchema.parse(req.params);
+    const product = await prisma.product;
+    const orders = await prisma.order.findMany({
+      where: { productId },
+      select: { id: true },
+    });
+    if (orders.length > 0) {
+      const orderIds = orders.map((order) => order.id);
+      console.error(`${productId} cannot be deleted.`);
+      console.error(
+        `This product has active orders (${orderIds.join(
+          ", "
+        )}). Please handle them before deleting the product.`
+      );
+    } else {
+      const product = await prisma.product.delete({
+        where: { id: productId },
+      });
+    }
+    send(res).ok({ msg: `Product deleted with ID: ${productId}` });
   })
 );
 
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import productsRouter from "./products";
+import ordersRouter from "./orders";
+import { defaultErrorHandler } from "./errors";
 
 const app = express();
 
@@ -10,6 +12,10 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/products", productsRouter);
+
+app.use("/orders", ordersRouter);
+
+app.use(defaultErrorHandler);
 
 app.get("/", async (req, res) => {
   res.status(200).json({ ok: "true", message: "hello" });
